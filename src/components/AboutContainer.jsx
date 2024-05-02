a257c184165557e4500438687be864f8b740df67
rafaelsgp <rafaelsgp@gmail.com>
2024-05-02 10:14:50 -0300

    first commit

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, unde
+        praesentium. Veniam sequi officia harum porro deserunt sunt quasi
+        delectus, enim, libero praesentium est pariatur! Natus dignissimos velit
+        voluptatum magnam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum obcaecati
+        vel fugit impedit illo sit tempora est laborum. Minima ab consequuntur
+        ipsa mollitia tempore sunt amet, quas non magni natus!
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi eligendi
+        itaque natus perspiciatis impedit suscipit iusto harum ad unde sit, cum
+        odit voluptates praesentium eveniet, nam libero! Odit, similique omnis.
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
